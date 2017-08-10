910248b445d626deeb8b9a0d15c9c25aca931506
jesusFx <fordas15@gmail.com>
2017-08-10 16:52:07 +0200

    Actualizada descripción

--- raton.sh
+++ raton.sh
@@ -1,12 +1,18 @@
+#! /bin/bash
+
+#Script para detección automática de ratón y deshabilitado del touchpad Synaptics
+
+#Requiere la previa modificación del fichero xorg.conf para activar el elemento del touchpad que recibe las señales externas
+
 while :
 do
-mousecount=`grep mouse /proc/bus/input/devices |grep Handler |wc -l`
-if [ "$mousecount" -eq "1" ]
-then
-	synclient TouchpadOff=0
-elif [ "$mousecount" -gt "1" ]
-then
-	synclient TouchpadOff=1
-fi
-sleep 0.25
+	mousecount=`grep mouse /proc/bus/input/devices |grep Handler |wc -l`
+	if [ "$mousecount" -eq "1" ]
+	then
+		synclient TouchpadOff=0
+	elif [ "$mousecount" -gt "1" ]
+	then
+		synclient TouchpadOff=1
+	fi
+	sleep 0.25
 done
