b100b6b492499f43a45515a83cf706bb1b097ec3
jesusFx <fordas15@gmail.com>
2017-12-29 16:44:10 +0100

    Script actualizado

--- checkUpdates.sh
+++ checkUpdates.sh
@@ -1,25 +1,33 @@
 #! /bin/bash
 
+sleep 5
+
+error=-1
+
+while [ $error -ne 0 ]; do
+	ping google.es -c 3
+	error=$?
+	if [ $error -ne 0 ]
+	then
+		notify-send --urgency=critical --expire-time=60000 "No hay conexión a Internet. Reintentando..."
+		sleep 30
+	fi
+done
+
 sudo apt-get update
 
 numUpdates=$(sudo apt-get --assume-no upgrade | grep -E "^\ \ " | wc -w)
 
 if [ $numUpdates -eq 0 ]
 then
-	notify-send --urgency=low --expire-time=15000 "No hay ninguna actualización disponible"
-fi
-
-if [ $numUpdates -lt 4 ] && [ $numUpdates -gt 0 ]
+	notify-send --urgency=low --expire-time=60000 "No hay ninguna actualización disponible"
+elif [ $numUpdates -lt 4 ] && [ $numUpdates -gt 0 ]
 then
-	notify-send --urgency=low --expire-time=15000 "Se han detectado $numUpdates actualizaciones"
-fi
-
-if [ $numUpdates -lt 10 ] && [ $numUpdates -gt 3 ]
+	notify-send --urgency=low --expire-time=60000 "Se han detectado $numUpdates actualizaciones"
+elif [ $numUpdates -lt 10 ] && [ $numUpdates -gt 3 ]
 then
-	notify-send --urgency=normal --expire-time=15000 "Se han detectado $numUpdates actualizaciones"
-fi
-
-if [ $numUpdates -lt 18 ] && [ $numUpdates -gt 9 ]
+	notify-send --urgency=normal --expire-time=60000 "Se han detectado $numUpdates actualizaciones"
+elif [ $numUpdates -lt 18 ] && [ $numUpdates -gt 9 ]
 then
-	notify-send --urgency=critical --expire-time=15000 "Se han detectado $numUpdates actualizaciones"
+	notify-send --urgency=critical --expire-time=60000 "Se han detectado $numUpdates actualizaciones"
 fi
