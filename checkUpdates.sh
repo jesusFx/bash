87f2376c12d8fff3825e811bdcd39d08028ef117
jesusFx <fordas15@gmail.com>
2018-01-05 19:49:28 +0100

    Script corregido

--- checkUpdates.sh
+++ checkUpdates.sh
@@ -1,38 +1,120 @@
 #! /bin/bash
 
-sleep 5
+sleep 2
 
 error=-1
 
-while [ $error -ne 0 ]; do
-	ping google.es -c 3
-	error=$?
-	if [ $error -ne 0 ]
+conexion_ok (){
+	while [ $error -ne 0 ]; do
+		ping google.es -c 2 2> /dev/null
+		error=$?
+		if [ $error -ne 0 ]
+		then
+			sudo killall -15 mate-notification-daemon 2> /dev/null
+			notify-send --urgency=critical --expire-time=60000 "No hay conexión a Internet. Reintentando..."
+			sleep 10
+		fi
+	done
+	sudo killall -15 mate-notification-daemon 2> /dev/null
+}
+
+sleep 1 && wmctrl -r "Actualizacion" -b add,above &
+
+(
+
+echo "# Buscando actualizaciones."
+
+conexion_ok
+
+notify-send --urgency=low --expire-time=60000 "Buscando actualizaciones..."
+
+estadoConex=-1
+
+while [ $estadoConex -lt 0 ];
+do
+	sudo apt-get update
+	if [ $? -eq 0 ]
 	then
-		notify-send --urgency=critical --expire-time=60000 "No hay conexión a Internet. Reintentando..."
-		sleep 30
+		estadoConex=1
 	fi
 done
 
-sudo apt-get update
+echo "25"
+echo "# Removiendo librerías sobrantes."
 
 sudo apt-get --assume-yes autoremove
 
+echo "50"
+echo "# Contando actualizaciones upgrade."
+
 numUpdates=$(sudo apt-get --assume-no upgrade | grep -E "^\ \ " | wc -w)
+
+echo "75"
+echo "# Contando actualizaciones dist-upgrade."
+
 numUpdates2=$(sudo apt-get --assume-no dist-upgrade | grep -E "^\ \ " | wc -w)
 
-let numtotal=numUpdates+numUpdates2
+estado=-1
 
-if [ $numtotal -eq 0 ]
+sudo killall -15 mate-notification-daemon 2> /dev/null
+
+if [ $numUpdates -eq 0 ] && [ $numUpdates2 -eq 0 ];
 then
+	echo "90"
+	echo "# Mostrando actualizaciones."
 	notify-send --urgency=low --expire-time=60000 "No hay ninguna actualización disponible"
-elif [ $numtotal -lt 4 ] && [ $numtotal -gt 0 ]
-then
-	notify-send --urgency=low --expire-time=60000 "Se han detectado $numtotal actualizaciones"
-elif [ $numtotal -lt 10 ] && [ $numtotal -gt 3 ]
-then
-	notify-send --urgency=normal --expire-time=60000 "Se han detectado $numtotal actualizaciones"
-elif [ $numtotal -lt 18 ] && [ $numtotal -gt 9 ]
-then
-	notify-send --urgency=critical --expire-time=60000 "Se han detectado $numtotal actualizaciones"
+	estado=1
 fi
+
+if [ $numUpdates -gt 0 ] && [ $numUpdates2 -eq 0 ] && [ $estado -lt 1 ];
+then
+	echo "90"
+	echo "# Mostrando actualizaciones."
+	notify-send --urgency=low --expire-time=60000 "Se han detectado $numUpdates actualizaciones upgrade"
+	estado=1
+fi
+
+if [ $numUpdates -eq 0 ] && [ $numUpdates2 -gt 0 ] && [ $estado -lt 1 ];
+then
+	echo "90"
+	echo "# Mostrando actualizaciones."
+	notify-send --urgency=low --expire-time=60000 "Se han detectado $numUpdates2 actualizaciones dist-upgrade/paquetes a eliminar"
+	estado=1
+fi
+
+if [ $numUpdates -gt 0 ] && [ $numUpdates2 -gt 0 ] && [ $estado -lt 1 ];
+then
+	if [ $numUpdates -lt 5 ] && [ $numUpdates -gt 0 ] && [ $estado -lt 1 ];
+	then
+		echo "90"
+		echo "# Mostrando actualizaciones."
+		notify-send --urgency=low --expire-time=60000 "Se han detectado $numUpdates actualizaciones upgrade"
+		notify-send --urgency=low --expire-time=60000 "Se han detectado $numUpdates2 actualizaciones dist-upgrade/paquetes a eliminar"
+		estado=1
+	elif [ $numUpdates -lt 12 ] && [ $numUpdates -gt 4 ] && [ $estado -lt 1 ];
+	then
+		echo "90"
+		echo "# Mostrando actualizaciones." ; sleep 4
+		notify-send --urgency=normal --expire-time=60000 "Se han detectado $numUpdates actualizaciones upgrade"
+		notify-send --urgency=normal --expire-time=60000 "Se han detectado $numUpdates2 actualizaciones dist-upgrade/paquetes a eliminar"
+		estado=1
+	elif [ $numUpdates -lt 60 ] && [ $numUpdates -gt 11 ] && [ $estado -lt 1 ];
+	then
+		echo "90"
+		echo "# Mostrando actualizaciones."
+		notify-send --urgency=critical --expire-time=60000 "Se han detectado $numUpdates actualizaciones upgrade"
+		notify-send --urgency=critical --expire-time=60000 "Se han detectado $numUpdates2 actualizaciones dist-upgrade/paquetes a eliminar"
+		estado=1
+	fi
+fi
+
+echo "# " ; sleep 2
+echo "99"
+
+echo "# Proceso finalizado." ; sleep 4
+echo "100"
+
+) |
+zenity --progress --title="Progress Status" --text="First Task." --percentage=0 --auto-close --auto-kill 2> /dev/null
+
+(( $? != 0 )) && zenity --error --text="Error in zenity command."
